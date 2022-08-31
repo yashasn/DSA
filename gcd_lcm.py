6b010dfe055d346db14817ee5b00bad511587b11
yashasn <yashasn14598@gmail.com>
2022-08-31 23:00:18 +0530

    lcm without gcd

--- gcd_lcm.py
+++ gcd_lcm.py
@@ -14,13 +14,24 @@ def FindGcd(n, m):
         n , m = m, n % m
     return n
 
+def FindLcm(n,m):
+    large = max(n,m)
+    small = min (n,m)
+    i = large
+    while(1):
+        if(i%small == 0):
+            return i
+        i += large
+    
+
+
 
 n = int(input("Enter first number:"))
 m = int(input("Enter second number:"))
 
 gcd = FindGcd(n,m)
-
-lcm = (n*m)/gcd
+lcm = FindLcm(n,m)
+#lcm = (n*m)/gcd
 
 print("GCD of %d and %d is %d" %(n,m,gcd))
 print("LCM of %d and %d is %d" %(n,m,lcm))
